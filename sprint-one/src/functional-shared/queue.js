d546a241daf10b3b650ec5ebd36d7d48d31b4841
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-04 16:40:17 -0700

    Started Prototypal Stack

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,7 +1,12 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var queue = { size_count : 0, storage : {}, head : 0, tail : 0};
+  var queue = {
+                size_count : 0,
+                storage : {},
+                head : 0,
+                tail : 0
+              };
   return _.extend(queue, queueMethods);
 };
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,23 @@
 var makeStack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var stack = Object.create(stackMethods);
+  stack.size_count = 0;
+  stack.storage = {};
+  return stack;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push : function(value) {
+
+  },
+  pop : function() {
+
+  },
+  size : function() {
+    return this.size_count;
+  }
+};
 
 
+var myStack = makeStack.create();
