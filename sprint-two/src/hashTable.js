ad9213c9452af54533ea4a78539b7bf9a01b6628
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-05 22:08:55 -0700

    hashTable passes

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,3 +1,5 @@
+
+
 var HashTable = function(){
   this._limit = 8;
   this._storage = makeLimitedArray(this._limit);
@@ -5,18 +7,34 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  if (this._storage[i]) {
+    var oldval = this._storage[i].original;
+    this._storage[i] = { original: oldval, collision: v};
+  } else {
+    this._storage[i] = { original: v, collision: v};
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-
+  if (this._storage[i] === null) {
+    return;
+  }
+  if (this._storage[i].original === k) {
+    return this._storage[i].original;
+  } else {
+    return this._storage[i].collision;
+  }
 };
 
 HashTable.prototype.remove = function(k){
-
+  var i = getIndexBelowMaxForKey(k, this._limit);
+  this._storage[i].original = null;
+  this._storage[i].collision = null;
 };
 
-
+// this works, but separate chaining only handles a load factor of 2
+// it would be educational to update handling a load factor of 10 (using an array)
 
 /*
  * Complexity: What is the time complexity of the above functions?
