a71e9758adb4c4a3c42c2aa22784093f68d2b0a6
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-05 20:03:47 -0700

    Linked List finished

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,37 @@ var makeLinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newnode = makeNode(value);
+    if (list.head === null) {
+      list.head = newnode;
+      list.tail = newnode;
+    } else {
+      var prevnode = list.tail;
+      list.tail = newnode;
+      prevnode.next = list.tail;
+    }
   };
 
-  list.removeHead = function(){
+  list.removeHead = function() {
+    if (list.head !== null) {
+      var result = list.head;
+      delete list.head;
+      list.head = result.next;
+      return result.value;
+    }
   };
 
   list.contains = function(target){
+    var result = false;
+    var nextnode = function(node) {
+      if ( node && node.value === target ) {
+        result = true;
+      } else if ( node && node.next ) {
+        nextnode(node.next);
+      }
+    };
+    nextnode(list.head);
+    return result;
   };
 
   return list;
