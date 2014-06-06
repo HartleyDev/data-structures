f62ef187060965122322a8ebe9c4fd097cafc6f8
Hack Reactor Students <mrwebbin@gmail.com>
2014-06-06 15:07:27 -0700

    Finished hashtable (draft)

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,30 +7,35 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage[i]) {
-    var oldval = this._storage[i].original;
-    this._storage[i] = { original: oldval, collision: v};
-  } else {
-    this._storage[i] = { original: v, collision: v};
+  if(this._storage.get(i)){
+    if (Array.isArray(this._storage[i])) {
+      this._storage[i].push([k,v]);
+    } else {
+      var og = this._storage.get(i);
+      this._storage[i] = [];
+      this._storage[i].push(og);
+      this._storage[i].push([k,v]);
+    }
+  }else {
+    this._storage.set(i,v);
   }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  if (this._storage[i] === null) {
-    return;
+  if (Array.isArray(this._storage[i])) {
+    for (var j = 0; j<this._storage[i].length ; j++) {
+      if (this._storage[i][j][0] === k) {
+        return this._storage[i][j][0];
+      }
+    }
   }
-  if (this._storage[i].original === k) {
-    return this._storage[i].original;
-  } else {
-    return this._storage[i].collision;
-  }
+  return this._storage.get(i);
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage[i].original = null;
-  this._storage[i].collision = null;
+  this._storage.set(i, null);
 };
 
 // this works, but separate chaining only handles a load factor of 2
